cb9aa1cb7497d2320f198fa035195a46e6caf3ce
RenanFG <55338512+RenanFG@users.noreply.github.com>
2019-11-26 17:24:33 -0300

    src/controller - cadastrarInstrumentos.php

---
+++ src/controller/cadastrarInstrumento.php
@@ -0,0 +1,13 @@
+<?php
+
+$ptipo = $_POST['ptipo'];
+$id = $_POST['pid'];
+$pnome = $_POST['pnome'];
+$pdesc = $_POST['pdesc'];
+$pimg = $_POST['pimg'];
+
+echo "<html><body><h1>"
+            .$ptipo."    ".$id."    ".$pnome."    ".$pdesc."
+            "</body></html>";
+
+ ?>
