9c73058d36e7064abc42369f09777ee56d8c9a38
Giovane <giovaneaalmeida27@gmail.com>
2019-11-26 19:17:04 -0300

    Criação do .ignore e reorganização das pastas

---
+++ Sistema SLIM/controller/cadastrarCliente.php
@@ -0,0 +1,36 @@
+<?PHP
+
+$nome = $_POST('cnome');
+$cpf = $_POST('ccpf');
+$email = $_POST('cemail');
+$senha = $_POST('csenha')
+$endereco = $_POST('cendereco');
+$cidade = $_POST('ccidade');
+$estado = $_POST('cestado');
+$telefone = $_POST('ctel');
+
+echo "<html><body>
+        <h1> ".$nome." ".$cpf." ".$email." ".$senha." ".$endereco." ".$cidade." ".$estado." ".$telefone."
+    </body>
+</html>;"
+
+$serverName = "localhost";
+$userName = "root";
+$password = "";
+$dataBase =
+
+$connection = mysqli_connect($serverName, $userName, $password, $dataBase);
+
+if(!$connection) {
+    die("Erro de conexão: ". $connection->connection_error);
+}
+
+$sql = "INSERT INTO cliente(Nome, Cpf, Email, Senha, Endereço, Cidade, Estado, Telefone) VALUES ('$nome','$cpf','$email','$senha','$endereco','$cidade','$estado','$telefone')"
+
+if(connection->query($sql) == TRUE) {
+    echo "Cliente salvo";
+} else {
+    echo "Erro ao cadastrar: " <br> .$connection->error;
+}
+
+?>
