9666f6418cf30c9a65951bd535e50de001076a64
RenanFG <55338512+RenanFG@users.noreply.github.com>
2019-11-26 17:26:06 -0300

    Add files via upload

--- src/controller/cadastrarInstrumento.php
+++ src/controller/cadastrarInstrumento.php
@@ -6,8 +6,23 @@ $pnome = $_POST['pnome'];
 $pdesc = $_POST['pdesc'];
 $pimg = $_POST['pimg'];
 
-echo "<html><body><h1>"
-            .$ptipo."    ".$id."    ".$pnome."    ".$pdesc."
-            "</body></html>";
+echo "<html><body><h1>
+            ".$ptipo."    ".$id."    ".$pnome."    ".$pdesc."</h1>
+            <img>".$pimg."</img> 
+            </body></html>;"
 
+$serverName = "localhost";
+$userName = "root";
+$password = "";
+$dataBase = "instrumentos";
+
+$connection = mysqli_connect($serverName, $userName, $password, $dataBase);
+
+if(!$connection) {
+    die("Erro de conexão: ". $connection->connection_error);
+}
+
+$sql = "INSERT INTO `instrumentos`(`ptipo`, `pnome`, `pdesc`, `pimg`, `pid`) VALUES ('$ptipo','$id','$pnome','$pdesc','$pimg')"
+ 
  ?>
+ 
