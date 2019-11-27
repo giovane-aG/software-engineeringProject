e1046007cf79f88de9919fa03cb8e748ace2ae12
RenanFG <55338512+RenanFG@users.noreply.github.com>
2019-11-27 17:16:00 -0300

    DAO instrumento

---
+++ Sistema SLIM/persistance/instrumentoDAO.php
@@ -0,0 +1,23 @@
+<?php
+class instrumentoDAO {
+    function _construct() {}
+    function salvar($instrumento, $conexao) {
+        $sql = "INSERT INTO instrumento(Tipo, Id, Nome, Desc) VALUES ('" .
+        $instrumento->getTipo() ."','" .
+        $instrumento->getId() ."','" .
+        $instrumento->getNome() ."','".
+        $instrumento->getDesc() ."')";
+        echo "<br>" . $sql;
+        if($conexao->query($sql) == TRUE) {
+            echo "Instrumento musical foi salvo no banco de dados com sucesso";
+        } else {
+            echo "Erro no cadastro: <br>".$conexao->error;
+        }
+    }
+    function consultarInstrumento($conexao) {
+        $sql = "SELECT Tipo, Id, Nome, Desc FROM instrumento";
+        $res = $conexao->query($sql);
+        return $res;
+    }
+}
+?>
