95e762b4471dadff6e5b57805ac069cb46f244da
RenanFG <55338512+RenanFG@users.noreply.github.com>
2019-11-28 15:24:26 -0300

    consultar instrumento

---
+++ Sistema SLIM/controller/consultarInstrumento.php
@@ -0,0 +1,47 @@
+<?php
+include_once '..\persistence\Connection.php';
+include_once '..\persistence\instrumentoDAO.php';
+$conexao = new Connection();
+$conn = $conexao->getConnection();
+$instrumentoDAO = new instrumentoDAO();
+$res = $clienteDAO->consultarInstrumentos($conn);
+if($res->num_rows > 0) {
+echo "<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
+</head>
+<body>
+    <table>
+        <tr>
+            <th>Tipo</th>
+            <th>Id</th>
+            <th>Nome</th>
+            <th>Desc</th>
+        </tr>";
+    while($registro = $res->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>".$registro['Tipo'] ."</td>".
+        "<td>".$registro['Id'] ."</td>".
+        "<td>".$registro['Nome'] ."</td>".
+        "<td>".$registro['Desc'] ."</td>".
+        "<br>";
+        echo"</tr>";
+    }
+    echo "</table></body></html>";
+}
+?>
