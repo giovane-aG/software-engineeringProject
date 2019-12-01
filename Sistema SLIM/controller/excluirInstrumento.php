faa3fb20a923bc52b4509eda667ae6e297aa56ee
RenanFG <55338512+RenanFG@users.noreply.github.com>
2019-12-01 03:52:42 -0300

    Add files via upload

---
+++ Sistema SLIM/controller/excluirInstrumento.php
@@ -0,0 +1,14 @@
+<?php
+include_once '..\persistence\Connection.php';
+include_once '..\persistence\instrumentoDAO.php';
+$conexao = new Connection();
+$conn = $conexao->getConnection();
+$instrumentoDAO = new instrumentoDAO();
+$res = $instrumentoDao -> excluirID($id, $conexao)
+if($res->num_rows = 1){
+    $registro = $res->fetch_assoc();
+}
+else{
+    echo "Erro ao deletar".$conexao->error;
+    }
+?>
