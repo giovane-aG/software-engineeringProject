6945c7713bf98a5b54f2a196eb92921ffb024dbb
RenanFG <55338512+RenanFG@users.noreply.github.com>
2019-12-01 18:48:05 -0300

    Add files via upload

--- Sistema SLIM/persistance/instrumentoDAO.php
+++ Sistema SLIM/persistance/instrumentoDAO.php
@@ -2,12 +2,12 @@
 class instrumentoDAO {
     function _construct() {}
     function salvar($instrumento, $conexao) {
-        $sql = "INSERT INTO instrumento(Tipo, Id, Nome, Preco, Desc) VALUES ('" .
-        $instrumento->getTipo() ."','" .
-        $instrumento->getId() ."','" .
+        $sql = "INSERT INTO instrumento.instrumento (Tipo, Id, Nome,Preco, Descr) VALUES ('" .
+        $instrumento->getTipo() ."','".
+        $instrumento->getId() ."','".
         $instrumento->getNome() ."','".
         $instrumento->getPreco() ."','".
-        $instrumento->getDesc() ."')";
+        $instrumento->getDescr() ."')";
         echo "<br>" . $sql;
         if($conexao->query($sql) == TRUE) {
             echo "Instrumento musical foi salvo no banco de dados com sucesso";
@@ -16,19 +16,27 @@ class instrumentoDAO {
         }
     }
     function consultarInstrumento($conexao) {
-        $sql = "SELECT Tipo, Id, Nome, Desc FROM instrumento";
+        $sql = "SELECT Tipo, Id, Nome, Preco, Descr FROM instrumento.instrumento";
         $res = $conexao->query($sql);
         return $res;
     }
     function consultarInstrumentoID($Id ,$conexao) {
-        $sql = "SELECT Tipo, Id, Nome, Desc FROM instrumento WHERE Id=".$Id;
+        $sql = "SELECT Tipo, Id, Nome, Preco, Descr FROM instrumento.instrumento WHERE Id=".$Id;
         $res = $conexao->query($sql);
         return $res;
     }
     function excluirID($Id ,$conexao) {
-        $sql = "DELETE FROM instrumento WHERE Id=".$Id;
-        $res = $conexao->query($sql);
+        $sql = "DELETE FROM instrumento WHERE Id=$Id";
+        $res= mysqli_query($conexao, $sql);
+        echo"$sql";
         return $res;
     }
-}
+
+    function alterarInstrumentoID($ins ,$conexao){
+        $tipo=$ins->getTipo(); $id= $ins->getId(); $nome = $ins->getNome(); $preco= $ins->getPreco(); $descr = $ins->getDescr();
+        $sql= "UPDATE instrumento SET Tipo='$tipo', Id= $id, Nome ='$nome', Preco ='$preco', Descr= '$descr' WHERE Id=$id";
+        $res= mysqli_query($conexao, $sql);
+        return $res;
+      }
+  }
 ?>
